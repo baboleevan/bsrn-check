9f896b37b36759ff9d22921b953682e728ba6fc7
baboleevan <leevan@gmail.com>
2019-04-16 18:47:53 +0900

    Update bsrn.js

--- bsrn.js
+++ bsrn.js
@@ -1,17 +1,18 @@
-	var checkCSRN = function(v) {
-		v = v.replace(/-/gi, '');
-		var nv = String(v), ts = 0, dg = 0, len = v.length, n = 1, i = 0, j = 0;
-		if (len == 13) {
-			for(; n < len; n++) {
-				i = n % 2;
-				j = 0;
-				if (i === 1) j = 1;
-				else if (i === 0) j = 2;
-				ts = ts + parseInt(nv.substring(n - 1, n), 10) * j;
+	var is_bsrn = function(v) {
+		v = String(v).replace(/-/gi, '');
+		if (v.length == 10) {
+			var sum = 0, arr = [1, 3, 7, 1, 3, 7, 1, 3, 5], tmp, last = v.charAt(9), _last;
+			for(var i=0; i < 9; i++) {
+				tmp = arr[i] * v[i];
+				if (i < 8) {
+					sum += tmp;
+				}
+				else {
+					sum += Number(String(tmp).charAt(0)) + Number(String(tmp).charAt(1));
+					_last = (10 - (sum % 10)) % 10;
+				}
 			}
-			dg = ts % 10;
-			if (dg !== 0) dg = 10 - dg;
-			return nv.substring(12, 13) === String(dg);
+			return _last == last;
 		}
 		return false;
 	};
